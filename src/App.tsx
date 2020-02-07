268fc80edac516df77e6b5649e09928b97560841
hasherr <eklein217@gmail.com>
2020-02-06 23:53:27 -0500

    Initial commit.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,49 @@
-import React from 'react';
+import React, { FormEvent, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import FormControl, { FormControlProps } from 'react-bootstrap/FormControl';
+import firebase, { auth } from 'firebase';
+import { FirebaseConfig } from './config';
 
 const App = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  if (!firebase.apps.length) {
+    firebase.initializeApp(FirebaseConfig);
+  }
+
+
+  const login = (event: object) => {
+    console.log(event);
+  }
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) : void => {
+    event.preventDefault();
+    event.stopPropagation();
+    
+    firebase.auth().createUserWithEmailAndPassword(email, password).then((result) => {
+      console.log(result);
+    });
+  }
+
   return (
     <div className="App">
       <header className="App-header">
+        <Form onSubmit={handleSubmit}>
+          <Form.Group>
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" onChange={(event: React.FormEvent) => { setEmail((event.currentTarget as any).value )}} />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Enter password" onChange={(event: React.FormEvent) => { setPassword((event.currentTarget as any).value )}} />
+          </Form.Group>
+          <Button type="submit">Log In</Button>
+        </Form>
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
