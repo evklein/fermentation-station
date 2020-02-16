abc517cf9ec9353205a966b6e447a219f3f20127
hasherr <eklein217@gmail.com>
2020-02-16 12:39:04 -0500

    Trying config again

---
+++ src/config.tsx
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyC402n30li_CIoC6KyvjgDpiIPX5k0s8b0",
+    authDomain: "fermstat-prod.firebaseapp.com",
+    databaseURL: "https://fermstat-prod.firebaseio.com",
+    projectId: "fermstat-prod",
+    storageBucket: "fermstat-prod.appspot.com",
+    messagingSenderId: "1044796804119",
+    appId: "1:1044796804119:web:10147ff96ff434ec55a052",
+    measurementId: "G-2P892VPTQJ"
+}
